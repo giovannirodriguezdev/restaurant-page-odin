243d4efc85456f7a886c86ade6b34f38fd931ec1
Giovanni Rodriguez <113161034+giovannirodriguezdev@users.noreply.github.com>
2025-05-22 23:30:30 -0700

    starter files added

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+
+const app = document.getElementById('app');
+app.innerHTML = `<h1>Welcome to My Restaurant</h1>`;
---
+++ webpack-config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
